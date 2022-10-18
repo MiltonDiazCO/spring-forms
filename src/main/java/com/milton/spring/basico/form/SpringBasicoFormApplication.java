c8d6ba8769f1d5c78737c631264b25a237b0c202
MiltonDiazCO <diazmartinezmilton@gmail.com>
2022-10-18 17:15:11 -0500

    Init commit

---
+++ src/main/java/com/milton/spring/basico/form/SpringBasicoFormApplication.java
@@ -0,0 +1,13 @@
+package com.milton.spring.basico.form;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicoFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBasicoFormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/milton/spring/basico/form/SpringBasicoFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milton.spring.basico.form;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicoFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
